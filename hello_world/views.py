48aed60a50c1cce990ef0165ab601cba1d0ab924
Michalina1405 <michalinakalenik@users.noreplay.github.com>
2019-04-05 11:52:38 +0200

    Change name

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Michalina!"
+moje_imie = "Michalina   !"
 msg = "Hello World!"
 
 @app.route('/')
